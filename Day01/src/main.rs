8425d8411c8f1e4aab7375a1050e9af0324a55a9
Rampagy <alexjsimon32@gmail.com>
2024-11-30 19:39:55 -0600

    Adding Day01

---
+++ Day01/src/main.rs
@@ -0,0 +1,30 @@
+use std::fs;
+
+#[allow(non_snake_case)]
+fn main() {
+    let contents: String = fs::read_to_string("src/input.txt").expect("Should have been abl;e to read the file");
+
+    println!("part 1: {}", part1(contents.clone()));
+    println!("part 2: {}", part2(contents.clone()));
+}
+
+
+fn part1(contents: String) -> u32 {
+    let mut somearray: Vec<char> = Vec::new();
+
+    for (row, line) in contents.lines().enumerate() {
+
+    }
+
+    return 0;
+}
+
+fn part2(contents: String) -> u32 {
+    let mut somearray: Vec<char> = Vec::new();
+
+    for (row, line) in contents.lines().enumerate() {
+
+    }
+
+    return 0;
+}
