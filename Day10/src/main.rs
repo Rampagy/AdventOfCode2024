a2a1313187173b7fb86d9e4381825b1f37495f3b
Alex S <simonalex@johndeere.com>
2024-12-10 16:35:39 -0600

    Adding day10 part 2 attempt

--- Day10/src/main.rs
+++ Day10/src/main.rs
@@ -1,5 +1,11 @@
+mod position;
+
 use std::fs;
 use std::time::Instant;
+use std::collections::{HashSet, HashMap};
+use ordered_float::OrderedFloat;
+use priority_queue::PriorityQueue;
+use position::{Position, PositionBuildHasher};
 
 
 #[allow(non_snake_case)]
@@ -20,11 +26,150 @@ fn main() {
 }
 
 #[allow(non_snake_case)]
+pub fn optimized_dijkstras_search(  weighted_map: &Vec<Vec<u8>>, start: Position, 
+                                    goal: Position ) -> Vec<Position> {
+    let mapWidth: usize = weighted_map[0].len();
+    let mapHeight: usize = weighted_map.len();
+
+    let mut path: Vec<Position> = Vec::with_capacity(1 as usize);
+    if start.x < 0 || start.y < 0 || goal.x >= mapWidth as i32 || goal.y >= mapHeight as i32 ||
+       start == goal || mapWidth < 2 || mapHeight < 2 {
+        return path;
+    }
+
+    /* Memory allocation */
+    let mut close_set: HashSet<Position, PositionBuildHasher> = HashSet::with_capacity_and_hasher(mapHeight * mapWidth, PositionBuildHasher);
+    let mut came_from: HashMap<Position, Position, PositionBuildHasher> = HashMap::with_capacity_and_hasher(mapHeight * mapWidth, PositionBuildHasher);
+    let mut gscore: HashMap<Position, f32, PositionBuildHasher> = HashMap::with_capacity_and_hasher(mapHeight * mapWidth, PositionBuildHasher);
+    let mut oheap: PriorityQueue<Position, OrderedFloat<f32>, PositionBuildHasher> = PriorityQueue::with_capacity_and_hasher(mapWidth + mapHeight, PositionBuildHasher);
+    let mut oheap_copy: HashMap<Position, f32, PositionBuildHasher> = HashMap::with_capacity_and_hasher(mapHeight * mapWidth, PositionBuildHasher);
+
+    let mut current: Position;
+    let mut neighbors: [Position; 4];
+
+    /* Add initial position to the search list */
+    gscore.insert(start, 0.0);
+
+    /* Note: gscore is multiplied by -1 before being entered into the oheap
+     *  because of how big of a pain in the ass it is to switch it from a
+     *  max heap to a min heap */
+    oheap.push(start, OrderedFloat::from(-1.0*(*gscore.get(&start).unwrap_or(&0.0))));
+    oheap_copy.insert(start, *gscore.get(&start).unwrap_or(&0.0));
+
+    let mut _count: u32 = 0;
+    while !oheap.is_empty() {
+        _count += 1;
+        (current, _) = oheap.pop().unwrap_or((Position::new(0,0), OrderedFloat::from(0.0)));
+        oheap_copy.remove(&current);
+        close_set.insert(current);
+
+        neighbors = current.get_surrounding_positions();
+
+        /* Search surrounding neighbors */
+        for neighbor in neighbors {
+            /* if the neighbor is a valid position */
+            if neighbor.x >= 0 && neighbor.y >= 0 && 
+                    neighbor.y < mapHeight as i32 && neighbor.x < mapWidth as i32 &&
+                    weighted_map[neighbor.y as usize][neighbor.x as usize] == weighted_map[current.y as usize][current.x as usize].saturating_add(1) {
+                let neighbor_gscore: f32 = *gscore.get(&current).unwrap_or(&0.0) + weighted_map[neighbor.y as usize][neighbor.x as usize] as f32 + 
+                                            optimized_heuristic(neighbor, current);
+
+                /* if the neighbor is already on the open list check to see if the neighbor is better before updating it*/
+                let in_open_list: bool = oheap_copy.contains_key(&neighbor);
+                if in_open_list && neighbor_gscore < *gscore.get(&neighbor).unwrap_or(&0.0){
+                    /* track the node's parent */
+                    came_from.insert(neighbor, current);
+
+                    /* gscore = cost to get from the start to the current position */
+                    gscore.entry(neighbor).and_modify(|val| *val = neighbor_gscore);
+
+                    /* update the neighbors values */
+                    oheap_copy.entry(neighbor).and_modify(|val| *val = neighbor_gscore);
+
+                    /* remove the old gscore */
+                    oheap.remove(&neighbor);
+
+                    /* Add the new fscore and sort */
+                    oheap.push(neighbor, OrderedFloat::from(-1.0*neighbor_gscore));
+                    continue;
+                }
+
+                /* check if it is on the closed list */
+                if close_set.contains(&neighbor) && neighbor_gscore < *gscore.get(&neighbor).unwrap_or(&0.0) {
+                    /* remove neighbor from closed list */
+                    close_set.remove(&neighbor);
+                }
+
+                /* Add to the open list */
+                if !close_set.contains(&neighbor) && !in_open_list {
+                    /* track the node's parent */
+                    came_from.insert(neighbor, current);
+
+                    /* gscore = cost to get rom the start to the current position */
+                    gscore.insert(neighbor, neighbor_gscore);
+
+                    /* add to the open list */
+                    oheap_copy.insert(neighbor, neighbor_gscore);
+                    oheap.push(neighbor, OrderedFloat::from(-1.0*neighbor_gscore));
+                }
+            }
+        }
+    }
+
+    /* trace path back from the goal */
+    current = goal;
+    while current != start {
+        path.push(current);
+        match came_from.get(&current) {
+            Some(x) => {
+                current = *x;
+            } None => {
+                break;
+            }
+        }
+    }
+
+    path.reverse();
+
+
+    return path;
+}
+
+
+#[inline]
+fn optimized_heuristic(a: Position, b: Position) -> f32 {
+    return (((a.x - b.x) + (a.y - b.y)) as f32).abs();
+}
+
+#[allow(non_snake_case)]
 fn part1(contents: String) -> u64 {
     let mut answer: u64 = 0;
+    let mut maze: Vec<Vec<u8>> = Vec::new();
+    let mut start_positions: Vec<Position> = Vec::new();
+    let mut end_positions: Vec<Position> = Vec::new();
 
-    for (_line_num, line) in contents.lines().enumerate() {
+    for (row_num, line) in contents.lines().enumerate() {
+        let mut row: Vec<u8> = Vec::new();
+        for (col_num, c) in line.chars().enumerate() {
+            if c == '0' {
+                start_positions.push(Position::new(col_num as i32, row_num as i32));
+            } else if c == '9' {
+                end_positions.push(Position::new(col_num as i32, row_num as i32));
+            }
 
+            row.push(c.to_digit(10).unwrap() as u8);
+        }
+        maze.push(row);
+    }
+
+    for start in start_positions {
+        for end in end_positions.clone() {
+            let path: Vec<Position> = optimized_dijkstras_search(&maze, start, end);
+
+            if path.len() == 9 {
+                answer += 1;
+            }
+        }
     }
 
     return answer;
@@ -32,11 +177,129 @@ fn part1(contents: String) -> u64 {
 
 
 #[allow(non_snake_case)]
+pub fn optimized_dijkstras_search_part2(weighted_map: &Vec<Vec<u8>>, 
+                                        start: Position, 
+                                        goal: Position) -> Vec<Vec<Position>> {
+
+    let mapWidth: usize = weighted_map[0].len();
+    let mapHeight: usize = weighted_map.len();
+
+    let mut paths: Vec<Vec<Position>> = Vec::new();
+    if start.x < 0 || start.y < 0 || goal.x >= mapWidth as i32 || goal.y >= mapHeight as i32 ||
+       start == goal || mapWidth < 2 || mapHeight < 2 {
+        return paths;
+    }
+
+    /* Memory allocation */
+    let mut close_set: HashSet<Position, PositionBuildHasher> = HashSet::with_capacity_and_hasher(mapHeight * mapWidth, PositionBuildHasher);
+    let mut came_from: HashMap<Position, Vec<Position>, PositionBuildHasher> = HashMap::with_capacity_and_hasher(mapHeight * mapWidth, PositionBuildHasher);
+    let mut oheap: PriorityQueue<Position, OrderedFloat<f32>, PositionBuildHasher> = PriorityQueue::with_capacity_and_hasher(mapWidth + mapHeight, PositionBuildHasher);
+
+    let mut current: Position;
+    let mut neighbors: [Position; 4];
+
+    /* Note: gscore is multiplied by -1 before being entered into the oheap
+     *  because of how big of a pain in the ass it is to switch it from a
+     *  max heap to a min heap */
+    oheap.push(start, OrderedFloat::from(0.0f32));
+
+    let mut _count: u32 = 0;
+    while !oheap.is_empty() {
+        _count += 1;
+        (current, _) = oheap.pop().unwrap_or((Position::new(0,0), OrderedFloat::from(0.0)));
+        close_set.insert(current);
+
+        neighbors = current.get_surrounding_positions();
+
+        /* Search surrounding neighbors */
+        for neighbor in neighbors {
+            /* if the neighbor is a valid position */
+            if neighbor.x >= 0 && neighbor.y >= 0 && 
+                    neighbor.y < mapHeight as i32 && neighbor.x < mapWidth as i32 &&
+                    weighted_map[neighbor.y as usize][neighbor.x as usize] == weighted_map[current.y as usize][current.x as usize].saturating_add(1) {
+
+                /* track the node's parent */
+                match came_from.get_mut(&neighbor) {
+                    Some(x) => {
+                        x.push(neighbor);
+                    } None => {
+                        came_from.insert(neighbor, vec![current]);
+                    }
+                }
+
+                /* remove the neighbor */
+                //oheap.remove(&neighbor);
+
+                /* Add the new item and sort */
+                oheap.push(neighbor, OrderedFloat::from(-0.0f32));
+            }
+        }
+    }
+
+
+    loop {
+        /* trace path back from the goal */
+        current = goal;
+        let mut path: Vec<Position> = Vec::new();
+
+        while current != start {
+            path.push(current.clone());
+            match came_from.get_mut(&current.clone()) {
+                Some(x) => {
+                    if x.len() > 1 {
+                        current = x.pop().unwrap();
+                    } else if x.len() > 0 {
+                        current = *x.first().unwrap();
+                    } else {
+                        break;
+                    }
+                } None => {
+                    break;
+                }
+            }
+        }
+
+        // TODO: the one before a duplicated number needs to be popped came_from
+        //path.dedup();
+        if path.len() == 9 {
+            path.reverse();
+            paths.push(path);
+        } else {
+            // no more paths to find
+            break;
+        }
+    }
+
+    return paths;
+}
+
+#[allow(non_snake_case)]
 fn part2(contents: String) -> u64 {
     let mut answer: u64 = 0;
+    let mut maze: Vec<Vec<u8>> = Vec::new();
+    let mut start_positions: Vec<Position> = Vec::new();
+    let mut end_positions: Vec<Position> = Vec::new();
 
-    for (_line_num, line) in contents.lines().enumerate() {
+    for (row_num, line) in contents.lines().enumerate() {
+        let mut row: Vec<u8> = Vec::new();
+        for (col_num, c) in line.chars().enumerate() {
+            if c == '0' {
+                start_positions.push(Position::new(col_num as i32, row_num as i32));
+            } else if c == '9' {
+                end_positions.push(Position::new(col_num as i32, row_num as i32));
+            }
 
+            row.push(c.to_digit(10).unwrap() as u8);
+        }
+        maze.push(row);
+    }
+
+    for start in start_positions {
+        for end in end_positions.clone() {
+            let paths: Vec<Vec<Position>> = optimized_dijkstras_search_part2(&maze, start, end);
+
+            answer += paths.len() as u64;
+        }
     }
 
     return answer;
@@ -56,6 +319,6 @@ mod tests {
     #[test]
     fn test_part2() {
         let contents: String = fs::read_to_string("src/test2.txt").expect("Should have been able to read the file");
-        assert_eq!(part2(contents.clone()), 2858);
+        assert_eq!(part2(contents.clone()), 81);
     }
 }
