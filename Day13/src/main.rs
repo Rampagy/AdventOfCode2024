e0ff54e818ed715684bd7a8514c0258bda0d8f90
Alex Simon <ajsimon@mtu.edu>
2024-12-14 06:06:22 -0600

    Day13 empty project

---
+++ Day13/src/main.rs
@@ -0,0 +1,71 @@
+mod position;
+
+use std::fs;
+use std::time::Instant;
+use std::collections::{HashMap, HashSet};
+use position::{Position, PositionBuildHasher};
+
+#[allow(non_snake_case)]
+fn main() {
+    let contents: String = fs::read_to_string("src/input.txt").expect("Should have been able to read the file");
+    let mut now: Instant;
+    let mut elapsed: std::time::Duration;
+
+    now = Instant::now();
+    let part1: u64 = part1(contents.clone());
+    elapsed = now.elapsed();
+
+    println!("part 1: {} ({:.2?})", part1, elapsed);
+
+    now = Instant::now();
+    let part2: u64 = part2(contents.clone());
+    elapsed = now.elapsed();
+
+    println!("part 2: {} ({:.2?})", part2, elapsed);
+}
+
+
+
+#[allow(non_snake_case)]
+fn part1(contents: String) -> u64 {
+
+    for (_line_num, line) in contents.lines().enumerate() {
+
+    }
+
+
+    let mut answer: u64 = 0;
+    return answer;
+}
+
+
+
+#[allow(non_snake_case)]
+fn part2(contents: String) -> u64 {
+
+    for (_line_num, line) in contents.lines().enumerate() {
+
+    }
+
+
+    let mut answer: u64 = 0;
+    return answer;
+}
+
+
+#[cfg(test)] #[allow(non_snake_case)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let contents: String = fs::read_to_string("src/test1.txt").expect("Should have been able to read the file");
+        assert_eq!(part1(contents.clone()), 480);
+    }
+
+    #[test]
+    fn test_part2() {
+        let contents: String = fs::read_to_string("src/test2.txt").expect("Should have been able to read the file");
+        assert_eq!(part2(contents.clone()), 772);
+    }
+}
