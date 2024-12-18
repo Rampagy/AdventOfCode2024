c6fb117fb2f1e2fc93e204fd79df20417f54df2f
Rampagy <alexjsimon32@gmail.com>
2024-12-18 15:45:50 -0600

    Adding Day16 part 1

--- Day16/src/main.rs
+++ Day16/src/main.rs
@@ -2,7 +2,7 @@ mod position;
 
 use std::fs;
 use std::time::Instant;
-use position::{Position, PositionBuildHasher};
+use position::Position;
 use std::collections::{HashSet, HashMap};
 use priority_queue::PriorityQueue;
 use ordered_float::OrderedFloat;
@@ -18,10 +18,10 @@ fn main() {
     let part1: u64 = part1(contents.clone());
     elapsed = now.elapsed();
 
-    println!("part 1: {} ({:.2?})", part1, elapsed); // 111488 - too high
+    println!("part 1: {} ({:.2?})", part1, elapsed);
 
     now = Instant::now();
-    let part2: u64 = part2(contents.clone()); 
+    let part2: u64 = part2(contents.clone());
     elapsed = now.elapsed();
 
     println!("part 2: {} ({:.2?})", part2, elapsed);
@@ -41,119 +41,108 @@ pub fn optimized_dijkstras_search(  weighted_map: &Vec<Vec<u8>>, start: Position
     }
 
     /* Memory allocation */
-    let mut close_set: HashSet<Position, PositionBuildHasher> = HashSet::with_capacity_and_hasher(mapHeight * mapWidth, PositionBuildHasher);
-    let mut came_from: HashMap<Position, Position, PositionBuildHasher> = HashMap::with_capacity_and_hasher(mapHeight * mapWidth, PositionBuildHasher);
-    let mut gscore: HashMap<Position, f32, PositionBuildHasher> = HashMap::with_capacity_and_hasher(mapHeight * mapWidth, PositionBuildHasher);
-    let mut oheap: PriorityQueue<Position, OrderedFloat<f32>, PositionBuildHasher> = PriorityQueue::with_capacity_and_hasher(mapWidth + mapHeight, PositionBuildHasher);
-    let mut oheap_copy: HashMap<Position, f32, PositionBuildHasher> = HashMap::with_capacity_and_hasher(mapHeight * mapWidth, PositionBuildHasher);
+    let mut close_set: HashSet<(Position, Position)> = HashSet::with_capacity(mapHeight * mapWidth);
+    let mut came_from: HashMap<(Position, Position), (Position, Position)> = HashMap::with_capacity(mapHeight * mapWidth);
+    let mut gscore: HashMap<(Position, Position), f32> = HashMap::with_capacity(mapHeight * mapWidth);
+    let mut oheap: PriorityQueue<(Position, Position), OrderedFloat<f32>> = PriorityQueue::with_capacity(mapWidth + mapHeight);
+    let mut oheap_copy: HashMap<(Position, Position), f32> = HashMap::with_capacity(mapHeight * mapWidth);
 
-    let mut current: Position;
+    let mut current: Position = start;
     let mut neighbors: [Position; 4];
+    let mut current_direction: Position = Position::new(1, 0);
 
     /* Add initial position to the search list */
-    gscore.insert(start, 0.0);
+    gscore.insert((current, current_direction), 0.0);
 
     /* Note: gscore is multiplied by -1 before being entered into the oheap
      *  because of how big of a pain in the ass it is to switch it from a
      *  max heap to a min heap */
-    oheap.push(start, OrderedFloat::from(-1.0*(*gscore.get(&start).unwrap_or(&0.0))));
-    oheap_copy.insert(start, *gscore.get(&start).unwrap_or(&0.0));
+    oheap.push((start, Position::new(1, 0)), 
+                OrderedFloat::from(-1.0*(*gscore.get(&(current, current_direction)).unwrap_or(&0.0))));
+    oheap_copy.insert((start, Position::new(1, 0)), 
+                *gscore.get(&(current, current_direction)).unwrap_or(&0.0));
 
     let mut _count: u32 = 0;
     while !oheap.is_empty() {
         _count += 1;
-        (current, _) = oheap.pop().unwrap_or((Position::new(0,0), OrderedFloat::from(0.0)));
-        oheap_copy.remove(&current);
-        close_set.insert(current);
+        ((current, current_direction), _) = oheap.pop().unwrap();
+        oheap_copy.remove(&(current, current_direction));
+        close_set.insert((current, current_direction));
 
-        neighbors = current.get_surrounding_positions();
+        if current == goal {
+            // exit
+            break;
+        }
+
+        if current == Position::new(12, 1) {
+            _count += 1;
+        }
 
         /* Search surrounding neighbors */
+        neighbors = current.get_surrounding_positions();
         for neighbor in neighbors {
+
             /* if the neighbor is a valid position */
             if neighbor.x >= 0 && neighbor.y >= 0 && 
                     neighbor.y < mapHeight as i32 && neighbor.x < mapWidth as i32 &&
                     weighted_map[neighbor.y as usize][neighbor.x as usize] < 255 {
-                let neighbor_gscore: f32 = *gscore.get(&current).unwrap_or(&0.0) + 
+
+                // track the neighbors current direction
+                let neighbor_direction: Position = neighbor - current;
+                let neighbor_gscore: f32 = *gscore.get(&(current, current_direction)).unwrap_or(&0.0) + 
                                             weighted_map[neighbor.y as usize][neighbor.x as usize] as f32 + 
                                             // use a default that it makes the start always point east to begin
                                             optimized_heuristic(
                                                 neighbor, 
                                                 current, 
-                                                *came_from.get(&current)
-                                                    .unwrap_or(&(start + Position::new(-1, 0)))
+                                                came_from.get(&(current, current_direction))
+                                                    .unwrap_or(&(start + Position::new(-1, 0), Position::new(-1, 0))).0
                                             );
 
-                /* if the neighbor is already on the open list check to see if the neighbor is better before updating it*/
-                let in_open_list: bool = oheap_copy.contains_key(&neighbor);
-                if in_open_list && neighbor_gscore < *gscore.get(&neighbor).unwrap_or(&0.0){
+                /* if the neighbor is already on the open list check to see if the neighbor is better before updating it */
+                let in_open_list: bool = oheap_copy.contains_key(&(neighbor, neighbor_direction));
+                if in_open_list && neighbor_gscore < *gscore.get(&(neighbor, neighbor_direction)).unwrap_or(&0.0){
                     /* track the node's parent */
-                    came_from.insert(neighbor, current);
+                    came_from.insert((neighbor, neighbor_direction), (current, current_direction));
 
                     /* gscore = cost to get from the start to the current position */
-                    gscore.entry(neighbor).and_modify(|val| *val = neighbor_gscore);
+                    gscore.entry((neighbor, neighbor_direction)).and_modify(|val| *val = neighbor_gscore);
 
                     /* update the neighbors values */
-                    oheap_copy.entry(neighbor).and_modify(|val| *val = neighbor_gscore);
+                    oheap_copy.entry((neighbor, neighbor_direction)).and_modify(|val| *val = neighbor_gscore);
 
                     /* remove the old gscore */
-                    oheap.remove(&neighbor);
+                    oheap.remove(&(neighbor, neighbor_direction));
 
                     /* Add the new fscore and sort */
-                    oheap.push(neighbor, OrderedFloat::from(-1.0*neighbor_gscore));
+                    oheap.push((neighbor, neighbor_direction), OrderedFloat::from(-1.0*neighbor_gscore));
                     continue;
                 }
 
                 /* check if it is on the closed list */
-                if close_set.contains(&neighbor) && neighbor_gscore < *gscore.get(&neighbor).unwrap_or(&0.0) {
+                if close_set.contains(&(neighbor, neighbor_direction)) && neighbor_gscore <= *gscore.get(&(neighbor, neighbor_direction)).unwrap_or(&0.0) {
                     /* remove neighbor from closed list */
-                    close_set.remove(&neighbor);
+                    close_set.remove(&(neighbor, neighbor_direction));
                 }
 
                 /* Add to the open list */
-                if !close_set.contains(&neighbor) && !in_open_list {
+                if !close_set.contains(&(neighbor, neighbor_direction)) && !in_open_list {
                     /* track the node's parent */
-                    came_from.insert(neighbor, current);
+                    came_from.insert((neighbor, neighbor_direction), (current, current_direction));
 
                     /* gscore = cost to get rom the start to the current position */
-                    gscore.insert(neighbor, neighbor_gscore);
+                    gscore.insert((neighbor, neighbor_direction), neighbor_gscore);
 
                     /* add to the open list */
-                    oheap_copy.insert(neighbor, neighbor_gscore);
-                    oheap.push(neighbor, OrderedFloat::from(-1.0*neighbor_gscore));
+                    oheap_copy.insert((neighbor, neighbor_direction), neighbor_gscore);
+                    oheap.push((neighbor, neighbor_direction), OrderedFloat::from(-1.0*neighbor_gscore));
                 }
             }
         }
     }
 
-    /* trace path back from the goal */
-    current = goal;
-    while current != start {
-        path.push(current);
-        current = *came_from.get(&current).unwrap_or(&Position::new(0,0));
-    }
-
-    path.reverse();
-
-    // print the result
-    for (row_num, row) in weighted_map.iter().enumerate() {
-        for (col_num, val) in row.iter().enumerate() {
-            if *val == 255 {
-                print!("{}",'#');
-            } else if start == Position::new(col_num as i32, row_num as i32) {
-                print!("{}", 'S')
-            } else if goal == Position::new(col_num as i32, row_num as i32) {
-                print!("{}", 'E');
-            } else if path.contains(&Position::new(col_num as i32, row_num as i32)) {
-                print!("{}", 'X');
-            } else {
-                print!(".");
-            }
-        }
-        println!();
-    }
-
-    return Some(gscore.get(&goal).unwrap().round() as u64);
+    let cost: Option<u64> = Some(gscore.get(&(current, current_direction)).unwrap().round() as u64);
+    return cost;
 }
 
 
@@ -162,6 +151,7 @@ fn optimized_heuristic(neighbor: Position, current: Position, previous: Position
     let step_cost: f32 = (((neighbor.x - current.x) + (neighbor.y - current.y)) as f32).abs();
     let turn_cost: f32 = if (previous.x - neighbor.x).abs() == 1 && 
                             (previous.y - neighbor.y).abs() == 1 { 1000.0 } // turning 
+                        else if previous == neighbor { 2000.0 } // did a 180
                         else { 0.0 }; // straight
 
     return step_cost + turn_cost;
@@ -194,14 +184,13 @@ fn part1(contents: String) -> u64 {
         map.push(row);
     }
 
-    let mut answer: u64 = optimized_dijkstras_search(&map, start, end).unwrap_or(0);
+    let answer: u64 = optimized_dijkstras_search(&map, start, end).unwrap_or(0);
     return answer;
 }
 
 
 #[allow(non_snake_case)]
 fn part2(contents: String) -> u64 {
-    let mut capture_map: bool = true;
     for (row_num, line) in contents.lines().enumerate() {
 
     }
@@ -225,5 +214,29 @@ mod tests {
     fn test_part1b() {
         let contents: String = fs::read_to_string("src/test1b.txt").expect("Should have been able to read the file");
         assert_eq!(part1(contents.clone()), 11048);
+    }
+
+    #[test]
+    fn test_part1c() {
+        let contents: String = fs::read_to_string("src/test1c.txt").expect("Should have been able to read the file");
+        assert_eq!(part1(contents.clone()), 3009);
+    }
+
+    #[test]
+    fn test_part1d() {
+        let contents: String = fs::read_to_string("src/test1d.txt").expect("Should have been able to read the file");
+        assert_eq!(part1(contents.clone()), 2009);
+    }
+
+    #[test]
+    fn test_part1e() {
+        let contents: String = fs::read_to_string("src/test1e.txt").expect("Should have been able to read the file");
+        assert_eq!(part1(contents.clone()), 6022);
+    }
+
+    #[test]
+    fn test_part1f() {
+        let contents: String = fs::read_to_string("src/test1f.txt").expect("Should have been able to read the file");
+        assert_eq!(part1(contents.clone()), 10040);
     }
 }
