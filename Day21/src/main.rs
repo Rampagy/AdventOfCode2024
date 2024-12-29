d30f7d086f2e468c1d6beacd178eb7017dce3749
Rampagy <alexjsimon32@gmail.com>
2024-12-29 16:15:02 -0600

    numpad implementation

--- Day21/src/main.rs
+++ Day21/src/main.rs
@@ -27,11 +27,90 @@ fn main() {
 
 #[allow(non_snake_case)]
 fn part1(contents: String) -> u64 {
+    let mut sequences: Vec<Vec<char>> = Vec::new();
 
     for (_row_num, line) in contents.lines().enumerate() {
-        // TODO
+        let mut row: Vec<char> = Vec::new();
+        for ch in line.chars() {
+            row.push(ch);
+        }
+
+        sequences.push(row);
     }
 
+    let numpad_positions: [Position; 11] = [ // 0,0 is top left
+        Position::new(1, 3), // 0
+        Position::new(0, 2), // 1
+        Position::new(1, 2), // 2
+        Position::new(2, 2), // 3
+        Position::new(0, 1), // 4
+        Position::new(1, 1), // 5
+        Position::new(2, 1), // 6
+        Position::new(0, 0), // 7
+        Position::new(1, 0), // 8
+        Position::new(2, 0), // 9
+        Position::new(2, 3), // A
+    ];
+
+    let mut dirpad_positions: HashMap<char, Position> = HashMap::new(); // 0,0 is top left
+    dirpad_positions.insert('A', Position::new(2, 0));
+    dirpad_positions.insert('^', Position::new(1, 0));
+    dirpad_positions.insert('>', Position::new(2, 1));
+    dirpad_positions.insert('v', Position::new(1, 1));
+    dirpad_positions.insert('<', Position::new(0, 1));
+
+
+    for seq in sequences {
+        let mut current_position: Position = Position::new(2, 3);
+        let mut numpad_path: Vec<char> = Vec::new();
+
+        for i in 0..seq.len() {
+            let numpad_next_char: char = *seq.get(i).unwrap();
+            let numpad_next_pos: Position = *numpad_positions.get(numpad_next_char.to_digit(10).unwrap_or(10) as usize).unwrap();
+            let position_delta: Position = numpad_next_pos - current_position;
+
+            // prioritize < over ^ over v over >
+            if position_delta.x < 0 {
+                // <
+                for _ in 0..position_delta.x.abs() {
+                    numpad_path.push('<');
+                }
+            }
+
+            if position_delta.y < 0 {
+                // ^
+                for _ in 0..position_delta.y.abs() {
+                    numpad_path.push('^');
+                }
+            }
+
+            if position_delta.y > 0 {
+                // v
+                for _ in 0..position_delta.y.abs() {
+                    numpad_path.push('v');
+                }
+            }
+
+            if position_delta.x > 0 {
+                // >
+                for _ in 0..position_delta.x.abs() {
+                    numpad_path.push('>');
+                }
+            }
+
+            numpad_path.push('A');
+            current_position = numpad_next_pos;
+        }
+
+        let mut out: String = String::new();
+        for ch in numpad_path {
+            out += ch.to_string().as_str();
+        }
+
+        println!("{}", out);
+
+        // TODO: direction pads
+    }
 
     return 0;
 }
