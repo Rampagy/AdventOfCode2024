875fabb0ab0c784b11a1bd059c1c2b1a09ba43d3
Rampagy <alexjsimon32@gmail.com>
2024-12-31 10:00:08 -0600

    day 22 part 2 niave brute force solution

--- Day22/README.md
+++ Day22/README.md
@@ -1,11 +1,14 @@
 # Day22
 
 ```
-
+part 1: 14180628689 (6.48ms)
+part 2: 1690 (160.27s)
 ```
 
 ## algorithm
 
-
+math and brute force
 
 ## notes/experimentation
+
+this would be a good solution for multi-threading but all the other solutions are single threaded so I decided to keep this one single threaded as well
--- Day22/src/main.rs
+++ Day22/src/main.rs
@@ -1,5 +1,10 @@
-use std::fs;
+use std::collections::HashSet;
+use std::{fs, u64};
 use std::time::Instant;
+
+
+const ITERATIONS: usize = 2000;
+const SEQUENCE_LENGTH: usize = 4;
 
 
 #[allow(non_snake_case)]
@@ -44,7 +49,7 @@ fn part1(contents: String) -> u64 {
     for (_row_num, line) in contents.lines().enumerate() {
         let mut secret: u64 = line.parse::<u64>().unwrap();
 
-        for _ in 0..2000 {
+        for _ in 0..ITERATIONS {
             secret = calc_next_secret(secret);
         }
         answer += secret;
@@ -54,15 +59,68 @@ fn part1(contents: String) -> u64 {
 }
 
 
+
 #[allow(non_snake_case)]
 fn part2(contents: String) -> u64 {
-    let mut answer: u64 = 0;
+    let mut secrets: Vec<[u64; ITERATIONS]> = Vec::new();
+    let mut potential_changes: HashSet<Vec<i8>> = HashSet::new();
+    let mut buyer_deltas_prices: Vec<Vec<i8>> = Vec::new();
 
     for (_row_num, line) in contents.lines().enumerate() {
+        let mut secret: u64 = line.parse::<u64>().unwrap();
+        let mut delta_prices: Vec<i8> = Vec::new();
+        let mut buyer_secrets: [u64; ITERATIONS] = [0; ITERATIONS];
+        buyer_secrets[0] = secret;
 
+        for i in 1..ITERATIONS {
+            secret = calc_next_secret(secret);
+            buyer_secrets[i] = secret;
+            delta_prices.push((secret%10) as i8 - (buyer_secrets[i-1]%10) as i8);
+
+            if i >= SEQUENCE_LENGTH {
+                // start populating potential_changes
+                potential_changes.insert(delta_prices[(i-SEQUENCE_LENGTH)..].to_vec());
+            }
+        }
+
+        // store the original buyer secrets
+        secrets.push(buyer_secrets);
+
+        // store the buyer deltas
+        buyer_deltas_prices.push(delta_prices);
     }
 
-    return answer;
+    // loop through each potential_change on all of the buyers and see which one produces the most money
+    let mut max_money: u64 = 0;
+    for potential in potential_changes {
+        let mut price_count: u64 = 0;
+        for (buyer_idx, buyer_price) in buyer_deltas_prices.clone().iter().enumerate() {
+            for i in SEQUENCE_LENGTH..ITERATIONS {
+                let mut all_idx_match: bool = true;
+                for j in 0..SEQUENCE_LENGTH {
+                    let idx: usize = i-j-1;
+                    if potential[SEQUENCE_LENGTH-j-1] != buyer_price[idx] {
+                        all_idx_match = false;
+                        break;
+                    }
+                }
+
+                if all_idx_match {
+                    // add the current bananas
+                    price_count += secrets[buyer_idx][i]%10;
+
+                    // skip the rest of the iterations for this buyer
+                    break;
+                }
+            }
+        }
+
+        if price_count > max_money {
+            max_money = price_count;
+        }
+    }
+
+    return max_money;
 }
 
 
@@ -81,6 +139,16 @@ mod tests {
     }
 
     #[test]
+    fn test_nextsecret_and_getones() {
+        let answers: [u64; 10] = [0, 6, 5, 4, 4, 6, 4, 4, 2, 4];
+        let mut secret: u64 = 123;
+        for i in 0..10 {
+            secret = calc_next_secret(secret);
+            assert_eq!(answers[i], secret%10);
+        }
+    }
+
+    #[test]
     fn test_part1a() {
         let contents: String = fs::read_to_string("src/test1a.txt").expect("Should have been able to read the file");
         assert_eq!(part1(contents.clone()), 37327623);
@@ -89,6 +157,6 @@ mod tests {
     #[test]
     fn test_part2a() {
         let contents: String = fs::read_to_string("src/test2a.txt").expect("Should have been able to read the file");
-        assert_eq!(part2(contents.clone()), 126384);
+        assert_eq!(part2(contents.clone()), 23);
     }
 }
