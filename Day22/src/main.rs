aa96c9e472197b17210f6d4326c43170d3ac10d5
Rampagy <alexjsimon32@gmail.com>
2024-12-31 07:58:33 -0600

    Day 22 part 1

--- Day22/src/main.rs
+++ Day22/src/main.rs
@@ -21,13 +21,33 @@ fn main() {
     println!("part 2: {} ({:.2?})", part2, elapsed);
 }
 
+#[allow(non_snake_case)]
+fn calc_next_secret(mut secret: u64) -> u64 {
+
+    // multiply by 64, xor with secret, then mod 16777216
+    secret = (secret ^ (secret << 6)) % 16777216;
+
+    // divide by 32, xor with secret, then mod 16777216
+    secret = (secret ^ (secret >> 5)) % 16777216;
+
+    // multiply by 2048, xor with secret, then mod 16777216
+    secret = (secret ^ (secret << 11)) % 16777216;
+
+    return secret;
+}
+
 
 #[allow(non_snake_case)]
 fn part1(contents: String) -> u64 {
     let mut answer: u64 = 0;
 
     for (_row_num, line) in contents.lines().enumerate() {
+        let mut secret: u64 = line.parse::<u64>().unwrap();
 
+        for _ in 0..2000 {
+            secret = calc_next_secret(secret);
+        }
+        answer += secret;
     }
 
     return answer;
@@ -51,9 +71,19 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_nextsecret() {
+        let answers: [u64; 10] = [15887950, 16495136, 527345, 704524, 1553684, 12683156, 11100544, 12249484, 7753432, 5908254];
+        let mut secret: u64 = 123;
+        for i in 0..10 {
+            secret = calc_next_secret(secret);
+            assert_eq!(answers[i], secret);
+        }
+    }
+
+    #[test]
     fn test_part1a() {
         let contents: String = fs::read_to_string("src/test1a.txt").expect("Should have been able to read the file");
-        assert_eq!(part1(contents.clone()), 126384);
+        assert_eq!(part1(contents.clone()), 37327623);
     }
 
     #[test]
