a9511d9976c0833d221ae29440a10f71fc579c6e
Alex S <simonalex@johndeere.com>
2025-01-03 10:57:18 -0600

    Making day 22 faster

--- Day22/src/main.rs
+++ Day22/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::{fs, u64};
 use std::time::Instant;
 
@@ -20,11 +20,12 @@ fn main() {
     println!("part 1: {} ({:.2?})", part1, elapsed);
 
     now = Instant::now();
-    let part2: u64 = part2(contents.clone());
+    let part2: u64 = part2(contents.clone(), ITERATIONS);
     elapsed = now.elapsed();
 
     println!("part 2: {} ({:.2?})", part2, elapsed);
 }
+
 
 #[allow(non_snake_case)]
 fn calc_next_secret(mut secret: u64) -> u64 {
@@ -59,53 +60,50 @@ fn part1(contents: String) -> u64 {
 }
 
 
-
 #[allow(non_snake_case)]
-fn part2(contents: String) -> u64 {
-    let mut secrets: Vec<[u64; ITERATIONS]> = Vec::new();
+fn part2(contents: String, iterations: usize) -> u64 {
     let mut potential_changes: HashSet<Vec<i8>> = HashSet::new();
-    let mut buyer_deltas_prices: Vec<Vec<i8>> = Vec::new();
+    let mut buyer_sequence_price_maps: Vec<HashMap<Vec<i8>, u8>> = Vec::new();
 
     for (_row_num, line) in contents.lines().enumerate() {
         let mut secret: u64 = line.parse::<u64>().unwrap();
+        let mut prev_secret: u64 = secret;
         let mut delta_prices: Vec<i8> = Vec::new();
-        let mut buyer_secrets: [u64; ITERATIONS] = [0; ITERATIONS];
-        buyer_secrets[0] = secret;
+        let mut sequence_price_map: HashMap<Vec<i8>, u8> = HashMap::new();
 
-        for i in 1..ITERATIONS {
+        for i in 1..iterations {
             secret = calc_next_secret(secret);
-            buyer_secrets[i] = secret;
-            delta_prices.push((secret%10) as i8 - (buyer_secrets[i-1]%10) as i8);
+            delta_prices.push((secret%10) as i8 - (prev_secret%10) as i8);
 
             if i >= SEQUENCE_LENGTH {
                 // start populating potential_changes
-                potential_changes.insert(delta_prices[(i-SEQUENCE_LENGTH)..].to_vec());
+                let seq: Vec<i8> = delta_prices[(i-SEQUENCE_LENGTH)..].to_vec();
+                potential_changes.insert(seq.clone());
+                if !sequence_price_map.contains_key(&seq.clone()) {
+                    sequence_price_map.insert(seq.clone(), (secret%10) as u8);
+                }
             }
+
+            prev_secret = secret;
         }
 
-        // store the original buyer secrets
-        secrets.push(buyer_secrets);
-
-        // store the buyer deltas
-        buyer_deltas_prices.push(delta_prices);
+        // store the sequence to price map
+        buyer_sequence_price_maps.push(sequence_price_map);
     }
 
     // loop through each potential_change on all of the buyers and see which one produces the most money
     let mut max_money: u64 = 0;
     for potential in potential_changes {
         let mut price_count: u64 = 0;
-        for (buyer_idx, buyer_price) in buyer_deltas_prices.clone().iter().enumerate() {
-            for i in SEQUENCE_LENGTH..ITERATIONS {
-                if potential[0] == buyer_price[i-4] && 
-                    potential[1] == buyer_price[i-3] && 
-                    potential[2] == buyer_price[i-2] && 
-                    potential[3] == buyer_price[i-1] {
-                        // add the current bananas
-                        price_count += secrets[buyer_idx][i]%10;
+        for (_buyer_idx, buyer_sequence_price_map) in buyer_sequence_price_maps.iter().enumerate() {
+            if let Some(price) = buyer_sequence_price_map.get(&potential) {
+                price_count += *price as u64;
+            }
 
-                        // skip the rest of the iterations for this buyer
-                        break;
-                }
+            if (price_count as usize + (buyer_sequence_price_maps.len().saturating_sub(_buyer_idx+1) * 9)) <= max_money as usize {
+                // if all the remaining indices were at max price it can't be greater than max_money
+                // therefore there is no point to continue searching this buyer or any of the rest
+                break;
             }
         }
 
@@ -151,6 +149,12 @@ mod tests {
     #[test]
     fn test_part2a() {
         let contents: String = fs::read_to_string("src/test2a.txt").expect("Should have been able to read the file");
-        assert_eq!(part2(contents.clone()), 23);
+        assert_eq!(part2(contents.clone(), ITERATIONS), 23);
+    }
+
+    #[test]
+    fn test_part2b() {
+        let contents: String = fs::read_to_string("src/test2b.txt").expect("Should have been able to read the file");
+        assert_eq!(part2(contents.clone(), 10), 6);
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# AdventOfCode2024
+# Advent of Code 2024
 
 https://adventofcode.com/2024
+
+![stars](AOCstars.jpg)
